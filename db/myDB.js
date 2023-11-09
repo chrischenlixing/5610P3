c2e9237dc2ac55407e1b6c3b7f173baae459e3f0
Lixing Chen <chrischenlixing@gmail.com>
2023-11-09 01:42:51 -0800

    url

--- db/myDB.js
+++ db/myDB.js
@@ -8,7 +8,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const url = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017";
-const client = new MongoClient(uri, {
+const client = new MongoClient(url, {
   userNewUrlParser: true,
   useUnifiedTopology: true,
 });
